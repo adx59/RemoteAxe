104a7076a3c7afc026986a0696340136a621c537
ShafinH <shafin1025@gmail.com>
2020-09-13 16:03:24 -0700

    Changed to ejs

--- web/app.js
+++ web/app.js
@@ -1,10 +1,10 @@
 var createError = require('http-errors'),
-    express = require('express'),
-    path = require('path'),
-    cookieParser = require('cookie-parser'),
-    logger = require('morgan'),
-    cors = require('cors'),
-    mongoose = require("mongoose");
+  express = require('express'),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  logger = require('morgan'),
+  cors = require('cors'),
+  mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(cors());
 
@@ -27,12 +27,12 @@ app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
