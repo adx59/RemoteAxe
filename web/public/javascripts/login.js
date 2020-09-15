9c44d7bba9d7505c641ea1cf291e0fdbb26217f6
adx59 <adamxu59@gmail.com>
2020-09-14 18:11:34 -0700

    fix login/registration switch

--- web/public/javascripts/login.js
+++ web/public/javascripts/login.js
@@ -1,18 +1,63 @@
+var registration = false;
+
 $(document).ready( () => {
     console.log("dom loaded");
     $("#submit").click((event) => {
-        $.ajax({
-            type: 'POST',
-            url: "/api/login",
-            dataType: 'json',
-            data: {
-                username: $("#username").val(),
-                password: $("#password").val()
-            }
-        }, (data) => {
-            console.log(data);
-        }, (err) => {
-            console.log(err);
-        })
+        switch (registration) {
+            case false:
+                $.ajax({
+                    type: 'POST',
+                    url: "/api/login",
+                    dataType: 'json',
+                    data: {
+                        username: $("#username").val(),
+                        password: $("#password").val()
+                    }
+                }, (data) => {
+                    console.log(data);
+                }, (err) => {
+                    console.log(err);
+                });
+                break;
+            case true:
+                if ($("#password").val() != $("#confPassword").val()) {
+                    alert("Passwords must match!")
+                } else {
+                    $.ajax({
+                        type: 'POST',
+                        url: "/api/register",
+                        dataType: 'json',
+                        data: {
+                            username: $("#username").val(),
+                            password: $("#password").val()
+                        }
+                    }, (data) => {
+                        console.log(data);
+                    }, (err) => {
+                        console.log(err);
+                    });
+                }
+                break;
+        }
+    });
+    $("#switcher").click((event) => {
+        switch (registration) {
+            case false:
+                registration = true;
+                $("#loginForm").append(
+                    "<input class=\"log\" type=\"password\" id=\"confPassword\" class=\"form-control mb-3\"\
+                    name=\"username\" placeholder=\"Confirm password\" required>"
+                );
+                $("#submit").html("Register");
+
+                break;
+            case true:
+                registration = false;
+                $("#confPassword").remove();
+                $("#submit").html("Login");
+
+                break;
+        }
+
     })
 });
